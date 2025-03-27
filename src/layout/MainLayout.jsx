54a8acd7de24cf639b0f15d3f5ce43c713625d53
jahir <nockjahirul@gmail.com>
2025-03-27 11:23:31 +0600

    add some logo or icon navbar and footer section

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../pages/shsred/Navbar';
+import Footer from '../pages/shsred/Footer';
 
 const MainLayout = () => {
     return (
         <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/shsred/Navbar.jsx
+++ src/pages/shsred/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext/AuthContext";
+import jobIcon from "../../assets/logo.png"
 
 const Navbar = () => {
     // for user nia asa login condation start>
@@ -52,7 +53,9 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">
+          <img className="w-12" src={jobIcon} alt="" />
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
